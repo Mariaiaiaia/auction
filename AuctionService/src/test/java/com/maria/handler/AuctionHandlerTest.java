dab72227b23b61ba52f5e57332af701343cb04c7
Maria Novitsky <mryhnwbyzqy@mryhs-MacBook-Air.local>
2025-07-19 10:09:30 +0300

    AuctionHandlerTest yaml updated

--- AuctionService/src/test/java/com/maria/handler/AuctionHandlerTest.java
+++ AuctionService/src/test/java/com/maria/handler/AuctionHandlerTest.java
@@ -38,7 +38,7 @@ public class AuctionHandlerTest {
     @BeforeAll
     void setUp() {
         auctionWebTestClient = WebTestClient.bindToServer()
-                .responseTimeout(Duration.ofSeconds(20))
+                .responseTimeout(Duration.ofSeconds(40))
                 .baseUrl("http://localhost:8084")
                 .build();
     }
