a74688c3279b60656a75aa79c69f1b53a710fbb7
maria <mariyanovitsky@gmail.com>
2025-07-28 23:22:51 +0300

    ci file updated

--- ItemService/src/main/java/com/maria/config/S3Config.java
+++ ItemService/src/main/java/com/maria/config/S3Config.java
@@ -2,15 +2,22 @@ package com.maria.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
+
+import java.net.URI;
 
 @Configuration
 public class S3Config {
     @Bean
     public S3AsyncClient s3Client() {
         return S3AsyncClient.builder()
-                .region(Region.EU_NORTH_1)
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create("test", "test")))
+                .region(Region.US_EAST_1)
                 .build();
     }
 }
